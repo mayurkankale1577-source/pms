b3366d31554f94912cc68f9a18c2e5a30065e894
Mayur Kankale <MayurK@sycasol>
2026-06-08 10:54:41 +0530

    dashboard file udated

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,16 +1,18 @@
 import Dashboard from "@/components/Dashboard";
 import { getCurrentUser } from "@/lib/current-user";
 import { getDashboardData } from "@/services/dashboard.service";
+import { redirect } from "next/navigation";
 
 export default async function DashboardPage() {
 
-  const user =
-    await getCurrentUser();
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect("/login");
+  }
 
   const data =
-    await getDashboardData(
-      user.id
-    );
+    await getDashboardData(user.id);
 
   return (
     <Dashboard
