2f58edc99eaed886d123a0382df2170e646042cf
Mayur Kankale <MayurK@sycasol>
2026-06-08 14:47:46 +0530

    date time zone change

--- src/services/dashboard.service.js
+++ src/services/dashboard.service.js
@@ -6,6 +6,7 @@ function formatTime(value) {
   return new Date(value).toLocaleTimeString(
     "en-IN",
     {
+      timeZone: "Asia/Kolkata",
       hour: "2-digit",
       minute: "2-digit",
       hour12: true,
